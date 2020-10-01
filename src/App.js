1ab7d184162c7af1cd6dfc220662a22188950670
ohad <ohad111@gmail.com>
2020-10-01 16:23:27 +0300

    Added api call for download queue

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ class App extends Component{
                 'base': process.env.REACT_APP_SONARR_API_URL + '/history?grabs',
                 'options': 'pageSize=',
                 'key': 'apikey=' + process.env.REACT_APP_SONARR_API_KEY
+            },
+            'queue': {
+                'base' : process.env.REACT_APP_SONARR_API_URL + '/queue?',
+                'key': 'apikey=' + process.env.REACT_APP_SONARR_API_KEY
             }
         }
         this.state = {
@@ -29,7 +33,7 @@ class App extends Component{
     }
 
     componentDidMount = () => {
-        this.GetAPIData()
+        this.getAPIData()
             .then(res => this.parseResults(res))
             .then(records => this.setState(records))
             .catch(err => console.log(err));
@@ -70,13 +74,15 @@ class App extends Component{
             }
         }
     }
-    GetAPIData = async () => {
+    getAPIData = async () => {
         let response = await Promise.all([
             fetch(this.createRequestString(this.apiObj.calendar, 7)),
             fetch(this.createRequestString(this.apiObj.history, 50))
         ]);
         return [await response[0].json(), await response[1].json()]
     }
+    getQueueData = async () => await fetch(this.createRequestString(this.apiObj.queue, null));
+
     getDate = range => {
         let now = new Date(),
             startString = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + (now.getDate());
@@ -89,14 +95,14 @@ class App extends Component{
         }
     }
     createRequestString = (obj, range) => {
-        return obj.hasOwnProperty('options') ?
-            obj.base + '&' + obj.options + range + '&' + obj.key :
-            obj.base + obj.key + '&start=' + this.getDate(range)[0] + '&end=' + this.getDate(range)[1];
+        if (range === null) return obj.base + obj.key;
+        if (obj.hasOwnProperty('options')) return obj.base + '&' + obj.options + range + '&' + obj.key;
+        return obj.base + obj.key + '&start=' + this.getDate(range)[0] + '&end=' + this.getDate(range)[1];
     }
     render() {
         return (
             <div className="App">
-                <TopBar/>
+                <TopBar queue={this.getQueueData}/>
                 <Shows shows={this.state.shows}/>
             </div>
         );
