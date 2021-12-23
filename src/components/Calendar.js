2210816d76c21ed40339af10f94903c6d6e0b249
ohadn <ohad111@gmail.com>
2021-12-23 20:44:53 +0200

    add text for days without any new shows

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -51,7 +51,33 @@ const Calendar = () => {
 
     const makeDateString = date => date.toLocaleString('default', {weekday: 'long', month: 'short', day: '2-digit' });
     const setItemClass = ({date, hasFile}) => hasFile ? 'hasFile' : (new Date(date) < new Date() ? 'missingFile' : null);
+    const plotDay = day => {
+        const shows = calendar.data.filter(dailyShows => makeDateString(new Date(dailyShows.date)) === day);
+        if (shows.length) {
+            return (
+                <ul key={day}>
+                    <h1>{day}</h1>
+                    {shows.map(show =>
+                        <li key={show.id} className={setItemClass(show)}>
+                            <a rel="noopener noreferrer" href={show.URL} target='_blank'><h2>{show.name}</h2></a>
+                            <p>Episode {show.episodeNumber}</p>
+                            <span>{new Date(show.date).toLocaleString('en-GB', {hourCycle: 'h24', timeStyle: 'short'})}</span>
+                        </li>
+                    )}
+                </ul>
+            )
+        } else {
+            return (
+                <ul key={day}>
+                    <h1>{day}</h1>
+                    <li className="no-show">
+                        <h2>No New Shows</h2>
+                    </li>
+                </ul>
+            )
+        }
 
+    };
     const plotWeek = () => {
         let day = new Date(),
             nextDay = new Date(day),
@@ -67,19 +93,7 @@ const Calendar = () => {
 
     return (
         <CalendarContainer>
-            {plotWeek().map(day =>
-                <ul key={day}>
-                    <h1>{day}</h1>
-                    {calendar.data.filter(dailyShows => makeDateString(new Date(dailyShows.date)) === day)
-                        .map(show =>
-                            <li key={show.id} className={setItemClass(show)}>
-                                <a rel="noopener noreferrer" href={show.URL} target='_blank'><h2>{show.name}</h2></a>
-                                <p>Episode {show.episodeNumber}</p>
-                                <span>{new Date(show.date).toLocaleString('en-GB', {hourCycle: 'h24', timeStyle: 'short'})}</span>
-                            </li>
-                        )}
-                </ul>
-            )}
+            { plotWeek().map(day => plotDay(day)) }
         </CalendarContainer>
     )
 };
